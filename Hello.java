8973243ca79957b8fcbafb17387935c29e40f9f7
headhuntez98 <43946896+headhuntez98@users.noreply.github.com>
2018-10-13 22:27:49 +0700

    tdtu branch revision 1

--- Hello.java
+++ Hello.java
@@ -3,8 +3,6 @@ class Hello
 {
 	public static void main(String[]args)
 	{
-		Scanner sc = new Scanner(System.in);
-		String inputName = sc.nextLine();
-		System.out.println("Hello World I am " + inputName);
+		System.out.println("Hello World I am TDTU ");
 	}
 }
