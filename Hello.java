5fc2ccc2b3bb11a6be8fdc3bba7e7f590ef4d190
unknown <hungphucnguyen1998@gmail.com>
2018-10-13 22:55:34 +0700

    master branch revision 5

--- Hello.java
+++ Hello.java
@@ -1,10 +1,28 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 class Hello
 {
 	public static void main(String[]args)
 	{
-		Scanner sc = new Scanner(System.in);
-		String inputName = sc.nextLine();
-		System.out.println("Hello World I am " + inputName);
+		String filename = "input.txt";
+		try{
+			
+			BufferedReader in = new BufferedReader(new FileReader(filename));  
+	        String inputFileStr;
+			int count = 0;
+			while ((inputFileStr = in.readLine()) != null)
+			{
+				System.out.println("Hello I am "+inputFileStr);
+				count++;
+			}
+			System.out.println("Total: "+count);
+			in.close();
+		}
+		catch (IOException e) {
+	        System.out.println("File read error for (" + filename + ")! ");
+	        e.printStackTrace();
+	    }
 	}
 }
